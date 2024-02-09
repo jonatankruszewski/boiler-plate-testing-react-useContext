e767f229cffcf8445f7efdbbc84cb8aa090aafed
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2024-02-09 19:52:42 +0200

    CSS

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,24 +1,25 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import styles from "./welcome.module.css";
 
 const Welcome = () => {
   const { logout, login, isLoggedin, user } = useContext(AuthContext);
 
   return (
-    <div>
-      {user && <div>Hello {user}</div>}
-      {!user && <div>Hello Anonymous Goose</div>}
+    <main className={styles.root}>
+      {user && <h1>Hello {user}!</h1>}
+      {!user && <h1>Hello Anonymous Goose</h1>}
       {!isLoggedin && (
-        <button aria-label="login" onClick={() => login("Jony")}>
+        <button aria-label="login" className={styles.button} onClick={() => login("Jony")}>
           Log In
         </button>
       )}
       {isLoggedin && (
-        <button aria-label="logout" onClick={() => logout()}>
+        <button aria-label="logout" className={styles.button} onClick={() => logout()}>
           Log out
         </button>
       )}
-    </div>
+    </main>
   );
 };
 
